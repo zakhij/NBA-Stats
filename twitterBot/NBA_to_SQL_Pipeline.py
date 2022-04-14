22b368c34d10cdbd819466ae72c800871d45da00
zhij713 <97318732+zhij713@users.noreply.github.com>
2022-04-14 15:41:01 -0700

    Update NBA_to_SQL_Pipeline.py

--- twitterBot/NBA_to_SQL_Pipeline.py
+++ twitterBot/NBA_to_SQL_Pipeline.py
@@ -1,3 +1,8 @@
+#Originally written in PyCharm. Class object created to facilitate the adding and updating of NBA stats data (both from CSVs and from nba_api package endpoints)
+#to the AWS RDS via mySQL connection. Currently, the only table that is stored and maintained in the database is 2021-22 season player statistics
+#from the LeagueDashPlayerStats NBA API endpoint. In the future, I will likely work to incorporate other tables related to player statistics, and perhaps even
+#expand to team, game, and lineup data. 
+
 import pymysql
 import sqlalchemy
 import pandas as pd
@@ -17,7 +22,11 @@ class NBAStatsToSQL:
         epFunction = f"ep.{endpoint.lower()}.{endpoint}().get_data_frames()[0]"
         epCall = eval(endpointFunction)
         epCall.to_sql(endpoint.lower(), self.engine, index=False)
-
+        if endpoint.lower() not in self.masterTables:
+                self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD PRIMARY KEY (`PLAYER_ID`);')
+                self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD FOREIGN KEY (`PLAYER_ID`) \
+                   REFERENCES leaguedashplayerstats(`PLAYER_ID`);')
+                
     def updateEndpoint(self,endpoint):
             epFunction = f"ep.{endpoint.lower()}.{endpoint}().get_data_frames()[0]"
             epCall = eval(epFunction)
@@ -32,11 +41,18 @@ class NBAStatsToSQL:
         tableName = url.split('/')[-1][:-4]
         readingCSV = pd.read_csv(url,index_col=0)
         readingCSV.to_sql(tableName,self.engine,index=False)
+        #Verify that the CSV has a PLAYER_ID column to ensure referential integrity 
+        self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD PRIMARY KEY (`PLAYER_ID`);')
+               self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD FOREIGN KEY (`PLAYER_ID`) \
+                  REFERENCES leaguedashplayerstats(`PLAYER_ID`);') 
 
     def updateCSV(self,url):
         tableName = url.split('/')[-1][:-4]
         readingCSV = pd.read_csv(url, index_col=0)
         self.engine.execute(f'DROP TABLE `{tableName}`;')
         readingCSV.to_sql(tableName, self.engine, index=False)
+        self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD PRIMARY KEY (`PLAYER_ID`);')
+               self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD FOREIGN KEY (`PLAYER_ID`) \
+                  REFERENCES leaguedashplayerstats(`PLAYER_ID`);') 
 
 NBAStatsToSQL().updateEndpoint('LeagueDashPlayerStats')
