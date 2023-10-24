48bd367c4dbb198ad1439d28b4112b9267ac390f
zhij713 <97318732+zhij713@users.noreply.github.com>
2023-10-24 11:50:38 -0700

    refactoring code: adding typing/modularity/error handling/etc

--- twitterBot/NBA_Stat_Tweeter_Bot.py
+++ twitterBot/NBA_Stat_Tweeter_Bot.py
@@ -1,48 +1,108 @@
-#DOONE
-#Originally written in PyCharm. Accesses player stats data stored in AWS RDS via mySQL connection (see NBA_to_SQL_Pipeline.py). Then scans Twitter mention tweets for names of current NBA players, replying to those tweets with the respective values for the number of minutes played this season for the players that were identified. 
-#This code has been uploaded on a crontab on an AWS EC2 instance, being executed every 10 minutes. 
+'''
+This file accesses player stats data stored in an AWS RDS server via mySQL 
+connection (see NBA_to_SQL_Pipeline.py), then scans Twitter mention tweets for 
+names of current NBA players, replying to those tweets with the respective values 
+for the number of minutes played this season for the players that were identified. 
+The code was uploaded on a crontab on an AWS EC2 instance, being executed every 10 min.
+For additional context, I was being blocked from accessing the API for live NBA statistics 
+(e.g., points scored, minuted played) using a cloud-based remote EC2 instance. As such, the
+RDS database is used as a proxy for storing NBA statistical data. It's not ideal, but
+this workaround was necessary to get the Twitter bot to function on a remote server.
+'''
 
-#In the future, as I'm able to grow what types of statistics are stored in the database, I may increase the scope of what type of information the reply tweet contains.
-
-import sqlalchemy
 import pandas as pd
 import tweepy
 from nba_api.stats.static import players
-from nba_api.stats.endpoints import playergamelog
+from typing import List, Dict
+import configparser
+import logging
+from NBA_to_SQL_Pipeline import NBAStatsToSQL
 
-user = "zhijaouy"
-pw = "" #Removed for privacy purposes
-host = "nbastatsaws1.cx08ymmfb8n8.us-east-1.rds.amazonaws.com"
-dbschema = "nbastats"
-engine = sqlalchemy.create_engine(f"mysql+pymysql://{user}:{pw}@{host}/{dbschema}")
-playerstatsDF = pd.read_sql("select * from leaguedashplayerstats", engine) #Extracting player stats data from SQL server
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
-apiKey = '' #Removed for privacy purposes
-apiSecret = '' #Removed for privacy purposes
-accessToken = '' #Removed for privacy purposes
-accessSecret = '' #Removed for privacy purposes
+def authenticate_twitter(apiKey: str, apiSecret: str, accessToken: str, accessSecret: str) -> tweepy.API:
+    """Authenticate with the Twitter API and return the API instance."""
+    try:
+        oauth = tweepy.OAuthHandler(apiKey, apiSecret)
+        oauth.set_access_token(accessToken, accessSecret)
+        return tweepy.API(oauth)
+    except Exception as e:
+        logging.error(f"Twitter auth failure: {e}")
+        return None  
 
-oauth = tweepy.OAuthHandler(apiKey, apiSecret)
-oauth.set_access_token(accessToken,accessSecret)
-print('Auth Success')
+def fetch_previous_tweet_replies(api: tweepy.API, count: int = 100) -> List[int]:
+    """Get the list of tweets that the bot has previously replied to."""
+    return [mytweet.in_reply_to_status_id for mytweet in api.user_timeline(count=count)]
 
-api = tweepy.API(oauth)
-playerDB = players.get_players()
+def find_players_in_tweet(tweet_text: str, playerDB: List[Dict[str, str]]) -> List[str]:
+    """Identify and return the NBA player's name mentioned in the tweet."""
+    found_names = []
+    for player in playerDB:
+        if player['full_name'].lower() in tweet_text.lower():
+            found_names.append(player['full_name'])
+    return found_names
 
-previousTweets = [mytweet.in_reply_to_status_id for mytweet in api.user_timeline(count=50)]
-for tweet in api.mentions_timeline():
-    if tweet.id not in previousTweets:
-        for pl in playerDB:
-            if pl['full_name'].lower() in tweet.text.lower():
-                name = pl['full_name']
-                print(f"Identified {name}")
-                nameMin = playerstatsDF.loc[playerstatsDF['PLAYER_NAME'] == name,
-                                            'MIN'].values[0].round(1) 
-                nameMinRank = playerstatsDF.loc[playerstatsDF['PLAYER_NAME'] == name, 'MIN_RANK'].values[0]
-                try:
-                    api.update_status(f"{name} played {nameMin} total minutes this season, "
-                                      f"which is rank {nameMinRank} out of all players this season!",
-                                  in_reply_to_status_id=tweet.id,auto_populate_reply_metadata=True)
-                    print(f"Successfully tweeted for {name}")
-                except Exception as e:
-                    print(e)
+def fetch_player_stats(player_name: str, statsDF: pd.DataFrame) -> (float, int):
+    """Fetch and return the minutes played and minutes rank of the player."""
+    minutes = statsDF.loc[statsDF['PLAYER_NAME'] == player_name, 'MIN'].values[0].round(1) 
+    rank = statsDF.loc[statsDF['PLAYER_NAME'] == player_name, 'MIN_RANK'].values[0]
+    return minutes, rank
+
+def reply_with_stats(api: tweepy.API, tweet_id: int, player_name: str, minutes: float, rank: int):
+    """Reply to the tweet with the player's minutes statistics."""
+    try:
+        api.update_status(f"{player_name} played {minutes} total minutes this season, "
+                          f"which is rank {rank} out of all players this season!",
+                          in_reply_to_status_id=tweet_id, auto_populate_reply_metadata=True)
+    except Exception as e:
+        logging.error(f"Failed to tweet for {player_name}: {e}")
+
+
+def main():
+    #Load configs
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    #Twitter API Authentication Setup
+    api_key = config['Twitter']['API_KEY']
+    api_secret = config['Twitter']['API_SECRET']
+    access_token = config['Twitter']['ACCESS_TOKEN']
+    access_secret = config['Twitter']['ACCESS_SECRET']
+    twitter_api = authenticate_twitter(api_key,api_secret,access_token,access_secret)
+
+    #Creating MySQL DB connection engine
+    user = config['Database']['USER']
+    password = config['Database']['PASSWORD']
+    host = config['Database']['HOST']
+    db_schema = config['Database']['SCHEMA']
+    nba_stats_instance = NBAStatsToSQL(user, password, host, db_schema)
+    engine = nba_stats_instance.engine
+
+    #Define structures for NBA player data
+    playerDB: List[Dict[str, str]] = players.get_players() #List of registered NBA players 
+    playerstatsDF: pd.DataFrame = pd.read_sql("select * from leaguedashplayerstats", engine) #Live stats of NBA players
+
+    already_replied = fetch_previous_tweet_replies(twitter_api)
+
+    #Going through tweets that mention the bot, replying when appropriate
+    for tweet in twitter_api.mentions_timeline():
+        if tweet.id in already_replied: #Avoid responding to tweets we've already replied to
+            continue
+        else:
+            found_player_names = find_players_in_tweet(tweet.text, playerDB)
+            for player in found_player_names:
+                mins_played, min_rank = fetch_player_stats(player, playerstatsDF)
+                reply_with_stats(twitter_api, tweet.id, player, mins_played, min_rank)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
--- twitterBot/NBA_to_SQL_Pipeline.py
+++ twitterBot/NBA_to_SQL_Pipeline.py
@@ -1,58 +1,79 @@
-#Originally written in PyCharm. Class object created to facilitate the adding and updating of NBA stats data (both from CSVs and from nba_api package endpoints)
-#to the AWS RDS via mySQL connection. Currently, the only table that is stored and maintained in the database is 2021-22 season player statistics
-#from the LeagueDashPlayerStats NBA API endpoint. In the future, I will likely work to incorporate other tables related to player statistics, and perhaps even
-#expand to team, game, and lineup data. 
+'''Class object created to facilitate the adding and updating of NBA stats data (both from CSVs and from nba_api package endpoints)
+to the AWS RDS via mySQL connection. Currently, the only table that is stored and maintained in the database is 2021-22 season player statistics
+from the LeagueDashPlayerStats NBA API endpoint. However, other tables can be incorporated, such as data tables related to player statistics, and perhaps even
+expand to team, game, and lineup data. Running the file updates the LeagueDashPlayerStats table.'''
 
 import pymysql
 import sqlalchemy
+from sqlalchemy.engine import Engine
 import pandas as pd
 from nba_api.stats import endpoints as ep
-
+from typing import List, Dict
+import configparser
 
 class NBAStatsToSQL:
-    def __init__(self):
-        self.user = "zhijaouy"
-        self.pw = " " #Removed for privacy purposes
-        self.host ="nbastatsaws1.cx08ymmfb8n8.us-east-1.rds.amazonaws.com" #mySQL Server Address
-        self.dbSchema = "nbastats"
-        self.masterTables = ["leaguedashplayerstats"] #Tables by which important identifiers such as PLAYER_ID originate from
-        self.engine = sqlalchemy.create_engine(f"mysql+pymysql://"f"{self.user}:{self.pw}@{self.host}/{self.dbSchema}")
+    def __init__(self, user: str, password: str, host: str, dbschema: str) -> None:
+        """Initialize the NBAStatsToSQL object with database credentials and setup the engine."""
+        self.user = user
+        self.pw = password
+        self.host = host
+        self.dbSchema = dbschema
+        self.masterTables = ['leaguedashplayerstats']
+        self.engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.user}:{self.pw}@{self.host}/{self.dbSchema}")
 
-    def uploadEndpoint(self,endpoint):
+    def _setup_keys(self, table_name: str) -> None:
+        """Sets up primary and foreign keys for the table if it's not a master table."""
+        if table_name not in self.masterTables:
+            self.engine.execute(f'ALTER TABLE `{table_name}` ADD PRIMARY KEY (`PLAYER_ID`);')
+            self.engine.execute(f'ALTER TABLE `{table_name}` ADD FOREIGN KEY (`PLAYER_ID`) \
+                                 REFERENCES leaguedashplayerstats(`PLAYER_ID`);') 
+
+    def uploadEndpoint(self, endpoint: str) -> None:
+        """Fetch data for a given NBA API endpoint and upload it to a corresponding SQL table."""
         epFunction = f"ep.{endpoint.lower()}.{endpoint}().get_data_frames()[0]"
-        epCall = eval(endpointFunction)
+        epCall = eval(epFunction)
         epCall.to_sql(endpoint.lower(), self.engine, index=False)
-        if endpoint.lower() not in self.masterTables:
-                self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD PRIMARY KEY (`PLAYER_ID`);')
-                self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD FOREIGN KEY (`PLAYER_ID`) \
-                   REFERENCES leaguedashplayerstats(`PLAYER_ID`);')
-                
-    def updateEndpoint(self,endpoint):
-            epFunction = f"ep.{endpoint.lower()}.{endpoint}().get_data_frames()[0]"
-            epCall = eval(epFunction)
-            self.engine.execute(f'DROP TABLE `{endpoint.lower()}`;')
-            epCall.to_sql(endpoint.lower(), self.engine, index=False)
-            if endpoint.lower() not in self.masterTables:
-                self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD PRIMARY KEY (`PLAYER_ID`);')
-                self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD FOREIGN KEY (`PLAYER_ID`) \
-                   REFERENCES leaguedashplayerstats(`PLAYER_ID`);') 
+        self._setup_keys(endpoint.lower())
 
-    def uploadCSV(self,url):
-        tableName = url.split('/')[-1][:-4]
-        readingCSV = pd.read_csv(url,index_col=0)
-        readingCSV.to_sql(tableName,self.engine,index=False)
-        #Verify that the CSV has a PLAYER_ID column to ensure referential integrity 
-        self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD PRIMARY KEY (`PLAYER_ID`);')
-               self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD FOREIGN KEY (`PLAYER_ID`) \
-                  REFERENCES leaguedashplayerstats(`PLAYER_ID`);') 
+    def updateEndpoint(self, endpoint: str) -> None:
+        """Update data in the table for a given NBA API endpoint."""
+        epFunction = f"ep.{endpoint.lower()}.{endpoint}().get_data_frames()[0]"
+        epCall = eval(epFunction)
+        self.engine.execute(f'DROP TABLE `{endpoint.lower()}`;')
+        epCall.to_sql(endpoint.lower(), self.engine, index=False)
+        self._setup_keys(endpoint.lower())
 
-    def updateCSV(self,url):
-        tableName = url.split('/')[-1][:-4]
+    def uploadCSV(self, url: str) -> None:
+        """Upload NBA stats data from a CSV file to a corresponding SQL table."""
+        tableName = url.split('/')[-1][:-4].lower()
+        readingCSV = pd.read_csv(url, index_col=0)
+        readingCSV.to_sql(tableName, self.engine, index=False)
+        self._setup_keys(tableName)
+
+    def updateCSV(self, url: str) -> None:
+        """Update data in an pre-existing SQL table from a CSV file."""
+        tableName = url.split('/')[-1][:-4].lower()
         readingCSV = pd.read_csv(url, index_col=0)
         self.engine.execute(f'DROP TABLE `{tableName}`;')
         readingCSV.to_sql(tableName, self.engine, index=False)
-        self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD PRIMARY KEY (`PLAYER_ID`);')
-               self.engine.execute(f'ALTER TABLE `{endpoint.lower()}` ADD FOREIGN KEY (`PLAYER_ID`) \
-                  REFERENCES leaguedashplayerstats(`PLAYER_ID`);') 
+        self._setup_keys(tableName)
 
-NBAStatsToSQL().updateEndpoint('LeagueDashPlayerStats')
+def main():
+    #Load configs
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    #Login to DB, initialize instance
+    user = config['Database']['USER']
+    password = config['Database']['PASSWORD']
+    host = config['Database']['HOST']
+    db_schema = config['Database']['SCHEMA']
+    db_instance = NBAStatsToSQL(user, password, host, db_schema)
+
+    #Update endpoint
+    db_instance.updateEndpoint('LeagueDashPlayerStats')
+
+if __name__ == '__main__':
+    main()
+
+    
