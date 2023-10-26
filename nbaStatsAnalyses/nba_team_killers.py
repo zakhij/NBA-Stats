92d830cf4e13359e5aad2b283e75acab6fe2ed25
zhij713 <97318732+zhij713@users.noreply.github.com>
2023-10-26 16:32:28 -0700

    refactoring

--- nbaStatsAnalyses/nba_team_killers.py
+++ nbaStatsAnalyses/nba_team_killers.py
@@ -1,6 +1,9 @@
-#Originally done in Pycharm. This code connects to NBA stats endpoints to determine which players "kill" certain teams, which means that they perform especially
-#well against a given team over their career. Points scored was used as the primary metric. For example, if Player A has a career PPG value of 12, but has 
-#played against the Los Angeles Lakers 8 times in his career and averaged 20, that player is considered a Lakers Killer. 
+"""
+This code connects to NBA stats endpoints to determine which players "kill" certain teams, 
+which means that they perform especially well against them over their career. Points scored 
+is used as the primary metric. For example, if Player A has a career PPG value of 12, but has 
+averaged 20 against the Lakers, that player is considered a Lakers Killer. 
+"""
 
 import pandas as pd
 pd.set_option('display.max_columns', None)
@@ -8,54 +11,67 @@ import time
 from nba_api.stats.static import players
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import playerdashboardbyyearoveryear
+from typing import Dict
 
 
-def Killer(playerName, teamName):
+def get_player_info():
+    """From NBA Stats API, players.get_players() returns a list of dict's, with each dict 
+    corresponding to a player. To make subsequent lookups easier, we return a dict where the
+    key is the player's full name, and the value is the dict full of player info.
+    """
     player = players.get_players()
+    player_dict = {p['full_name'].lower(): p for p in player}
+    return player_dict
+
+def get_team_info():
+    """From NBA Stats API, team.get_teams() returns a list of dict's, with each dict 
+    corresponding to a team. To make subsequent lookups easier, we return a dict where the
+    key is the team's name, and the value is the dict full of team info.
+    """
     team = teams.get_teams()
-    pl = [p for p in player if p['full_name'].lower() in playerName.lower()][0]
-    pName = pl['full_name']
-    pID = pl['id']
+    team_dict = {t['full_name'].lower(): t for t in team}
+    return team_dict
 
-    tm = [t for t in team if t['full_name'].lower() in teamName.lower()][0]
-    tName = tm['full_name']
-    tID = tm['id']
+def get_career_stats_against_team(player_id:int, team_id:int) -> Dict[str, float]:
+    """Given the player and team, digs through the player's career PPG overall and specifically 
+    against that team, returning a dict that contains both PPG values"""
+    player_career_YoY = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(player_id).get_data_frames()[1]
+    player_opp_YoY = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(player_id, opponent_team_id=team_id).get_data_frames()[1]
+    
+    player_stats = {}
+    player_stats['overall_ppg'] = round(player_career_YoY['PTS'].sum()/player_career_YoY['GP'].sum(),1)
+    player_stats['ppg_against_team'] = round(player_opp_YoY['PTS'].sum()/player_opp_YoY['GP'].sum(),1)
+    player_stats['gp_against_team'] =  player_opp_YoY['GP'].sum()
 
-    playerCareerYoY = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(pID).get_data_frames()[1]
-    playerOppYoY = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(pID, opponent_team_id=tID).get_data_frames()[1]
-    gameThreshold = playerOppYoY['GP'].sum() >= 5
-    if gameThreshold:
-        careerPPG = playerCareerYoY['PTS'].sum()/playerCareerYoY['GP'].sum()
-        oppPPG = playerOppYoY['PTS'].sum()/playerOppYoY['GP'].sum()
-        ppgThreshold = careerPPG * 1.25 < oppPPG and oppPPG >= 5
-        #careerPPM = playerCareerYoY['PTS'].sum()/playerOppYoY['']
-        print(f"{pName} averages {round(careerPPG,1)} points for his career, but against"
-              f" the {tName}, he has averaged {round(oppPPG,1)} PTS.")
-        if ppgThreshold:
-            print("He's a killer!")
+    return player_stats
+
+def check_if_killer(player_stats:Dict, game_threshold:int = 5, better_threshold:int = 1.25) -> bool:
+    """Checks if a player is a killer, based on a game and performance increase threshold"""
+    if player_stats['ppg_against_team'] >= player_stats['overall_ppg'] * better_threshold\
+    and player_stats['gp_against_team'] >= game_threshold:
+        return True
     else:
-        print('Not enough games played against that team, sorry!')
+        return False
 
-def teamKillers(teamName):
-    player = players.get_players()
-    team = teams.get_teams()
-    pl = [p for p in player if p['is_active']]
-    tm = [t for t in team if t['full_name'].lower() in teamName.lower()][0]
-    tID = tm['id']
-    killerList = []
-    for p in pl:
-        playerCareerYoY = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(p['id']).get_data_frames()[1]
-        playerOppYoY = playerdashboardbyyearoveryear.PlayerDashboardByYearOverYear(p['id'], opponent_team_id=tID).get_data_frames()[1]
-        gameThreshold = playerOppYoY['GP'].sum() >=5
-        if gameThreshold:
-            careerPPG = playerCareerYoY['PTS'].sum()/playerCareerYoY['GP'].sum()
-            oppPPG = playerOppYoY['PTS'].sum()/playerOppYoY['GP'].sum()
-            ppgThreshold = careerPPG * 1.25 < oppPPG and oppPPG >= 5
-            if ppgThreshold:
-                killerList.append(p['full_name'])
-            time.sleep(1.5)
-    return killerList
+def express_stats(player_name:str, team_name:str, player_stats:Dict) -> None:
+    """Prints out the stored stats of the player performance"""
+    print(f"{player_name} has averaged {player_stats['overall_ppg']} points for his career, and against\
+    the {team_name}, he has averaged {player_stats['ppg_against_team']} points in {player_stats['gp_against_team']} games.")
 
-Killer('Klay Thompson', 'Sacramento Kings')
 
-print(teamKillers('Sacramento Kings'))
+def main():
+    player_name = input("Enter player name: ")
+    team_name = input("Enter team name: ")
+
+    player_info = get_player_info()
+    player_id = player_info.get(player_name.lower())['id']
+    team_info = get_team_info()
+    team_id = team_info.get(team_name.lower())['id']
+
+    stats = get_career_stats_against_team(player_id, team_id)
+    express_stats(player_name, team_name, stats)
+    if check_if_killer(stats):
+        print(f"He's a certified {team_name} killer!")
+
+
+main()
